60365b6e48ae401c344c9016734c063656bea6a0
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-15 12:01:17 +0100

    Implemented Context API using ContextComponent.Consumer to consume the data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBqvwqZjKLxH7nuRaU7GfKeJNimeSlnA7M",
+  authDomain: "crwn-db-4dc15.firebaseapp.com",
+  databaseURL: "https://crwn-db-4dc15.firebaseio.com",
+  projectId: "crwn-db-4dc15",
+  storageBucket: "crwn-db-4dc15.appspot.com",
+  messagingSenderId: "352810866522",
+  appId: "1:352810866522:web:bad1907e028ed721350563",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +29,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
